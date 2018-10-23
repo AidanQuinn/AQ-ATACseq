720fed285802ac749ca2d1af3d13b44be285e3fb
Aidan Quinn <quinn0219@gmail.com>
2018-10-23 11:34:32 -0400

    Update run_samtools_clean.sh
    Speed up, request more threads (tuned samtools)

--- run_samtools_clean.sh
+++ run_samtools_clean.sh
@@ -6,18 +6,18 @@ for s in `cat ./samples.txt`; do
     echo "Cleaning up alignment ${s}:"
     # sort by name
     echo "Sorting ${s}..."
-    samtools sort -n -@ 24 -m 10G -o ./aligned/${s}_mm10_nsorted.bam ./aligned/${s}_mm10.bam
+    samtools sort -n -@ 23 -m 10G -o ./aligned/${s}_mm10_nsorted.bam ./aligned/${s}_mm10.bam
     echo "Fixing mate-pair flags ${s}..."
-    samtools fixmate -@ 24 -r ./aligned/${s}_mm10_nsorted.bam ./aligned/${s}_mm10_nsorted_fixed.bam
+    samtools fixmate -@ 23 -r ./aligned/${s}_mm10_nsorted.bam ./aligned/${s}_mm10_nsorted_fixed.bam
     echo "Rmoving PCR duplicates ${s}..."
     samtools rmdup -S ./aligned/${s}_mm10_nsorted_fixed.bam ./aligned/${s}_mm10_nsorted_fixed_dedup.bam
     echo "Sorting by coordinate ${s}..."
-    samtools sort -@ 24 -m 10G -o ./aligned/${s}_mm10_nsorted_fixed_dedup_psorted.bam ./aligned/${s}_mm10_nsorted_fixed_dedup.bam
+    samtools sort -@ 23 -m 10G -o ./aligned/${s}_mm10_nsorted_fixed_dedup_psorted.bam ./aligned/${s}_mm10_nsorted_fixed_dedup.bam
     echo "Removing intermediate files ${s}..."
     rm -f ./aligned/${s}_mm10.bam
     rm -f ./aligned/${s}_mm10_nsorted.bam
     rm -f ./aligned/${s}_mm10_nsorted_fixed.bam
     rm -f ./aligned/${s}_mm10_nsorted_fixed_dedup.bam
     echo "Indexing ${s}_mm10_nsorted_fixed_dedup_psorted.bam..."
-    samtools index ./aligned/${s}_mm10_nsorted_fixed_dedup_psorted.bam
+    samtools index -@ 23 ./aligned/${s}_mm10_nsorted_fixed_dedup_psorted.bam
 done
